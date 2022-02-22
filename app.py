03ce9f7548b46a587b3379fc2cc661b22cf68c83
Yurii Paniv <mr.robinhad@gmail.com>
2022-02-23 01:36:22 +0200

    Add sample chatbot

---
+++ app.py
@@ -0,0 +1,26 @@
+import random
+import gradio as gr
+
+
+def chat(message, history):
+    history = history or []
+    if message.startswith("How many"):
+        response = random.randint(1, 10)
+    elif message.startswith("How"):
+        response = random.choice(["Great", "Good", "Okay", "Bad"])
+    elif message.startswith("Where"):
+        response = random.choice(["Here", "There", "Somewhere"])
+    else:
+        response = "I don't know"
+    history.append((message, response))
+    return history, history
+
+
+iface = gr.Interface(
+    chat,
+    ["text", "state"],
+    ["chatbot", "state"],
+    allow_screenshot=False,
+    allow_flagging="never",
+)
+iface.launch()
